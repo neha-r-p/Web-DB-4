8f9ba03252bc993371855b54f6fdb3ffc53f842b
Neha Patel <neha.r.p@gmail.com>
2019-09-12 13:42:48 -0700

    created index.js

--- knexfile.js
+++ knexfile.js
@@ -4,10 +4,17 @@ module.exports = {
 
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true, 
     connection: {
-      filename: './dev.sqlite3'
+      filename: './data/database.db3',
     },
-    useNullAsDefault: true
+    // needed when using foreign keys
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+      },
+    },
   },
 
 };
